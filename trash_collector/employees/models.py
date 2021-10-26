842d062a84482db5634d9c38511cc4e26ea23431
Micheal Moore <michealmoore@Micheals-MacBook-Air.local>
2021-10-26 07:18:30 -0400

    Added edit profile for employye, and fixed bug to finish making employee account

--- trash_collector/employees/models.py
+++ trash_collector/employees/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # TODO: Create an Employee model with properties required by the user stories
 class Employee(models.Model):
     name = models.CharField(max_length=50)
-    user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.SET_NULL)
+    user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
     address = models.CharField(max_length=50)                         
     zipcode = models.CharField(max_length=5)
 
--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -5,17 +5,33 @@ from django.urls import reverse
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.decorators import login_required
 from datetime import date
+
 from .models import Employee
 
 # Create your views here.
 
 # TODO: Create a function for each path created in employees/urls.py. Each will need a template as well.
 
-
+@login_required
 def index(request):
-    # This line will get the Customer model from the other app, it can now be used to query the db for Customers
-    Customer = apps.get_model('customers.Customer')
-    return render(request, 'employees/index.html')
+    # The following line will get the logged-in user (if there is one) within any view function
+    logged_in_user = request.user
+    try:
+        # This line will return the customer record of the logged-in user if one exists
+        logged_in_employee = Employee.objects.get(user=logged_in_user)
+        today = date.today()
+        logged_in_employee_zipcode = logged_in_employee.zip_code
+        Customer = apps.get_model('customers.Customer')
+        todays_customers = Customer.objects.filter(zip_code=logged_in_employee_zipcode)
+        
+        context = {
+            'todays_customers': todays_customers
+        }
+        return render(request, 'employees/index.html', context)
+    except ObjectDoesNotExist:
+        return HttpResponseRedirect(reverse('employees:create'))
+
+    
 
 @login_required
 def create(request):
@@ -25,8 +41,28 @@ def create(request):
         address_from_form = request.POST.get('address')
         zip_from_form = request.POST.get('zip_code')
         new_employee = Employee(name=name_from_form, user=logged_in_user,
-                                address=address_from_form, zip_code=zip_from_form)
+                                address=address_from_form, zipcode=zip_from_form)
         new_employee.save()
         return HttpResponseRedirect(reverse('employees:index'))
     else:
         return render(request, 'employees/create.html')
+
+
+@login_required
+def edit_profile(request):
+    logged_in_user = request.user
+    logged_in_employee = Employee.objects.get(user=logged_in_user)
+    if request.method == "POST":
+        name_from_form = request.POST.get('name')
+        address_from_form = request.POST.get('address')
+        zip_from_form = request.POST.get('zipcode')
+        logged_in_employee.name = name_from_form
+        logged_in_employee.address = address_from_form
+        logged_in_employee.zip_code = zip_from_form
+        logged_in_employee.save()
+        return HttpResponseRedirect(reverse('employees:index'))
+    else:
+        context = {
+            'logged_in_employee': logged_in_employee
+        }
+        return render(request, 'employees/edit_profile.html', context)
